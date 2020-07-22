2c8b3199369237e235b14ef1c282773833b7bc8c
Damien Cassou <damien@cassou.me>
2020-07-22 21:25:32 +0200

    Facilitate usage

---
+++ default.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import
+  (fetchTarball "http://nixos.org/channels/nixos-19.09/nixexprs.tar.xz") {
+    inherit system;
+  }, system ? builtins.currentSystem }:
+
+let
+  node = pkgs."nodejs-10_x";
+  nodePackages = import ./node2nix/override.nix { inherit pkgs; };
+  pieChartModule = nodePackages.package;
+in pkgs.writeShellScriptBin "piechart" ''
+  ${node}/bin/node ${pieChartModule}/lib/node_modules/piechart/index.js "$@"
+''
--- launch.sh
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env nix-shell
-#! nix-shell -i bash --packages nodejs-10_x nix
-#! nix-shell -I nixpkgs=channel:nixos-19.09
-
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-node $(nix-build -I nixpkgs=channel:nixos-19.09 $DIR/override.nix -A package)/lib/node_modules/piechart/index.js "$@"
