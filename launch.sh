70907fde271a064b53bcddad5d32f8fba6ac8350
Damien Cassou <damien@cassou.me>
2020-07-22 11:50:16 +0200

    Make sure to use the override.nix from the script

--- launch.sh
+++ launch.sh
@@ -2,4 +2,6 @@
 #! nix-shell -i bash --packages nodejs-10_x nix
 #! nix-shell -I nixpkgs=channel:nixos-19.09
 
-node $(nix-build -I nixpkgs=channel:nixos-19.09 override.nix -A package)/lib/node_modules/piechart/index.js "$@"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+node $(nix-build -I nixpkgs=channel:nixos-19.09 $DIR/override.nix -A package)/lib/node_modules/piechart/index.js "$@"
